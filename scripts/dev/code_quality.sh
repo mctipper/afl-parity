3031a2da678124b7f96d1624f29abfcfaadc805b
mctipper <morgan.tipper@hotmail.com>
2025-03-08 10:34:45 +0000

    fix: format then typecheck

--- scripts/dev/code_quality.sh
+++ scripts/dev/code_quality.sh
@@ -3,14 +3,6 @@
 # exit status so other scripts can hook into this output (not implemented...)
 exit_status=0
 
-echo "Running mypy to check for type errors..."
-mypy .
-if [ $? -ne 0 ]; then
-    exit_status=1
-fi
-
-echo ""
-
 echo "Running ruff to check for linting issues..."
 ruff check
 if [ $? -ne 0 ]; then
@@ -24,5 +16,15 @@ ruff format
 if [ $? -ne 0 ]; then
     exit_status=1
 fi
+
+echo ""
+
+echo "Running mypy to check for type errors..."
+mypy .
+if [ $? -ne 0 ]; then
+    exit_status=1
+fi
+
+
 
 exit $exit_status
