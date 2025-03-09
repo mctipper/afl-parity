b0532c50da0146b17a43e000d49d6cc889748250
mctipper <morgan.tipper@hotmail.com>
2025-03-09 22:18:56 +0000

    fix: absolute paths duh

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -5,33 +5,40 @@ BRANCH="main"
 YEAR=$(date +"%Y")
 DOCKER_CONTAINER_NAME="afl-parity-container"
 
+# dir
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+BASE_DIR="$SCRIPT_DIR/.."
+LOG_DIR="$BASE_DIR/.logs"
+OUTPUT_DIR="$BASE_DIR/output"
+
 # logging
 CURRENT_TIME=$(date +"%Y%m%d_%H%M%S")
-LOG_FILE="./.logs/${CURRENT_TIME}_${YEAR}_run_docker.log"
-
-# directory management
-if [ ! -d "./output" ]; then
-  echo "Creating ./output directory..."
-  mkdir ./output
-fi
-
-if [ ! -d "./.logs" ]; then
-  echo "Creating ./.logs directory..."
-  mkdir ./.logs
-fi
-
+LOG_FILE="$LOG_DIR/${CURRENT_TIME}_${YEAR}_run_docker.log"
 log_with_datetime() {
   local message="$1"
   local current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
   echo "[$current_datetime] $message" | tee -a "$LOG_FILE"
 }
 
+# directory management
+if [ ! -d "$OUTPUT_DIR" ]; then
+  log_with_datetime "Creating $OUTPUT_DIR directory..."
+  mkdir -p "$OUTPUT_DIR"
+fi
+
+if [ ! -d "$LOG_DIR" ]; then
+  log_with_datetime "Creating $LOG_DIR directory..."
+  mkdir -p "$LOG_DIR"
+fi
+
+
 push_to_github() {
+  cd "$BASE_DIR" || exit
   # check for changes before/after add
   git checkout $BRANCH
   git pull origin $BRANCH
   STATUS_BEFORE=$(git status --porcelain)
-  git add ./output/
+  git add "$OUTPUT_DIR/"
   STATUS_AFTER=$(git status --porcelain)
 
   if [[ "$STATUS_BEFORE" != "$STATUS_AFTER" ]]; then
@@ -47,11 +54,11 @@ push_to_github() {
   fi
 }
 
+
 check_first_hamiltonian_cycle_already_exists() {
-  FILE_PATH="./output/$YEAR/${YEAR}_dfs_traversal_output.json"
+  FILE_PATH="$OUTPUT_DIR/${YEAR}_dfs_traversal_output.json"
   
-  # check if file exists and if so insepct if a hamiltonian cycle is present
-  # to prevent pointless traversing again and again...
+  # check if file exists and if so inspect if a hamiltonian cycle is present
   if [ -f "$FILE_PATH" ]; then
     FIRST_HAMILTONIAN_CYCLE=$(jq '.first_hamiltonian_cycle' "$FILE_PATH")
     if [ "$FIRST_HAMILTONIAN_CYCLE" == "null" ]; then
@@ -60,9 +67,10 @@ check_first_hamiltonian_cycle_already_exists() {
       return 0  # "first_hamiltonian_cycle" is not null
     fi
   else
-    return 1 # json file does not exist
+    return 1  # JSON file does not exist
   fi
 }
+
 
 check_container_status() {
   if docker ps --filter "name=$DOCKER_CONTAINER_NAME" | grep "$DOCKER_CONTAINER_NAME" > /dev/null 2>&1; then
